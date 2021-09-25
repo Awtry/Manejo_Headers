6e8d5cebadd3ca1a2588c211e67c9c8625a8ef1d
Awtry <jorge_verah@outlook.es>
2021-09-24 21:23:16 -0500

    Get anti tronar

--- src/main/java/web/Servlet.java
+++ src/main/java/web/Servlet.java
@@ -10,6 +10,119 @@ import javax.servlet.http.*;
 public class Servlet extends HttpServlet {
 
     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String opcion = request.getParameter("opcion");
+        String valFor1 = request.getParameter("a");
+        String valFor2 = request.getParameter("b");
+        String valFor3 = request.getParameter("c");
+        String resFor1 = request.getParameter("res1");
+        String resFor2 = request.getParameter("res2");
+
+        switch (opcion) {
+            case "1":
+                //ALAN
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<title>Formula general</title>");
+                out.print("<script type='text/javascript' src='recursos/calculos.js'></script>");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<h1>Resultados: </h1>");
+                //action='/Manejo_Headers/ServletHeader' method='post' Va antes del onSubmit3
+                //onsubmit='return calcularGeneral()'
+                out.print("<form>");
+                out.print("<table width='200' id='enfasis-columnas'><tr>");
+                out.print("<td class='columna'>Valor A:</td><td><tr style='text-align: center'>" + valFor1 + "</td></tr>");
+                out.print("<td class='columna'>Valor B:</td><td><tr style='text-align: center'>" + valFor2 + "</td></tr>");
+                out.print("<td class='columna'>Valor C:</td><td><<tr style='text-align: center'>" + valFor3 + "</td></tr>");
+                //out.print("<tr style='text-align: center'>" + "<td>" + "<button class='default' onClick='calcularGeneral()'> Calcular </button>" + "</td>" + "</tr>");
+                out.print("<td class='columna'>Valor C:</td><td><<tr style='text-align: center'>" + resFor1 + "</td></tr>");
+                out.print("<td class='columna'>Valor C:</td><td><<tr style='text-align: center'>" + resFor2 + "</td></tr>");
+
+               // out.print("<tr style='text-align: center'><td class'columna' id='res1'>Resultado 1: </td></tr>");
+               //out.print("<tr style='text-align: center'><td class'columna' id='res2'>Resultado 2: </td></tr>");
+                out.print("<br>");
+                out.print("</body>");
+                out.print("</html>");
+                out.close();
+                break;
+
+            case "2":
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<title>Números primos</title>");
+                out.print("<script type='text/javascript' src='recursos/calculo.js'></script>");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<h1>Números primos: </h1>");
+                out.print("<h4>Dame el número: </h4>");
+                out.print("<form action='/Manejo_Headers/ServletH' method='post' onsubmit='return validarForma(this)'>");
+                out.print("<table width='200' id='enfasis-columnas'><tr><td class='columna'>Número: </td>");
+                out.print("<td><input type'text' name='val_1' value ='primNum' onfocus='this.select()'></td><tr>");
+                out.print("<td class='columna'>Resultado: </td><td><input type='text' name='txt_resultadoPRIM' onfocus='this.select()'></td><tr>");
+                out.print("<tr style='text-align: center'><td><input type='submit' value='Vamonos' class='default'></td></tr>");
+
+                out.print("</table></form>");
+
+                out.print("</body>");
+                out.print("</html>");
+                out.close();
+                break;
+
+            case "3":
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<title>Triángulo</title>");
+                out.print("<script type='text/javascript' src='recursos/calculo.js'></script>");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<h1>Tipo de triángulo</h1>");
+                out.print("<h4>Introduce los valores: </h4>");
+                out.print("<form action='/Manejo_Headers/ServletH' method='post' onsubmit='return validarForma(this)'>");
+                out.print("<table width='200' id='enfasis-columnas'><tr>");
+                out.print("<td class='columna'>Lado A: </td><td><input type'text' name='val_1' value ='val_1' onfocus='this.select()'></td><tr>");
+                out.print("<td class='columna'>Lado B: </td><td><input type='text' name='val_2' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Lado C:</td><td><input type='text' name='val_3' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Resultado: </td><td><input type='text' name='txt_resultado' onfocus='this.select()'></td><tr>");
+                out.print("<tr style='text-align: center'><td><input type='submit' value='Vamonos' class='default'></td></tr>");
+
+                out.print("</table></form>");
+
+                out.print("</body>");
+                out.print("</html>");
+                out.close();
+
+                break;
+            default:
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<title>Formula general</title>");
+                out.print("<script type='text/javascript' src='recursos/calculo.js'></script>");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<h1>Formula general: </h1>");
+                out.print("<h4>Resultados Finales: </h4>");
+                out.print("<form action='/Manejo_Headers/ServletH' method='post' onsubmit='return validarForma(this)'>");
+                out.print("<table width='200' id='enfasis-columnas'><tr><td class='columna'>Valor A: </td>");
+                out.print("<td><input type'text' name='val_1' value = 'primer valor' onfocus='this.select()'></td><tr>");
+                out.print("<td class='columna'>Valor B: </td><td><input type='text' name='val_2' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Valor C:</td><td><input type='text' name='val_3' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Resultado: </td><td><input type='text' name='txt_resultado' onfocus='this.select()'></td><tr>");
+                out.print("<tr style='text-align: center'><td><input type='submit' value='Vamonos' class='default'></td></tr>");
+
+                out.print("</table></form>");
+
+                out.print("</body>");
+                out.print("</html>");
+                out.close();
+                break;
+        }
+    }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
@@ -20,7 +133,7 @@ public class Servlet extends HttpServlet {
 
         switch (opcion) {
             case "1":
-                out.print("<html>");
+                /* out.print("<html>");
                 out.print("<head>");
                 out.print("<title>Formula general</title>");
                 out.print("<script type='text/javascript' src='recursos/calculo.js'></script>");
@@ -30,14 +143,37 @@ public class Servlet extends HttpServlet {
                 out.print("<h4>Introduce los valores: </h4>");
                 out.print("<form  method='post' '>");
                 out.print("<table width='200' id='enfasis-columnas'><tr>");
-                out.print("<td class='columna'>Valor A: </td><td><input type'text' name='val_1' onfocus='this.select()'></td><tr>");
-                out.print("<td class='columna'>Valor B: </td><td><input type='text' name='val_2' onfocus='this.select()'></td></tr><tr>");
-                out.print("<td class='columna'>Valor C:</td><td><input type='text' name='val_3' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Valor A: </td><td><input type'text' name='val_1' id='val_1' onfocus='this.select()'></td><tr>");
+                out.print("<td class='columna'>Valor B: </td><td><input type='text' name='val_2'id='val_2' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Valor C:</td><td><input type='text' name='val_3' id='val_3' onfocus='this.select()'></td></tr><tr>");
                 out.print("<td class='columna'>Resultado: </td><td><input type='text' name='resForm' value = "+ resFormula +" onfocus='this.select()'></td><tr>");
                 out.print("<tr style='text-align: center'><td><button onClick='Formula()' value='Calcula' class='default'>Maiu </button></td></tr>");
 
                 out.print("</table></form>");
 
+                out.print("</body>");
+                out.print("</html>");
+                out.close();*/
+
+                //ALAN
+                out.print("<html>");
+                out.print("<head>");
+                out.print("<title>Formula general</title>");
+                out.print("<script type='text/javascript' src='recursos/calculos.js'></script>");
+                out.print("</head>");
+                out.print("<body>");
+                out.print("<h1>Dame los valores: </h1>");
+                //action='/Manejo_Headers/ServletHeader' method='post' Va antes del onSubmit3
+                //onsubmit='return calcularGeneral()'
+                out.print("<form>");
+                out.print("<table width='200' id='enfasis-columnas'><tr>");
+                out.print("<td class='columna'>Valor A:</td><td><input type'text' name='val_1' id='val_1' onfocus='this.select()'></td><tr>");
+                out.print("<td class='columna'>Valor B:</td><td><input type='text' name='val_2' id='val_2' onfocus='this.select()'></td></tr><tr>");
+                out.print("<td class='columna'>Valor C:</td><td><input type='text' name='val_3' id='val_3' onfocus='this.select()'></td></tr><tr>");
+                out.print("<tr style='text-align: center'>" + "<td>" + "<button class='default' onClick='calcularGeneral()'> Calcular </button>" + "</td>" + "</tr>");
+                out.print("<tr style='text-align: center'><td class'columna' id='res1'>Resultado 1: </td></tr>");
+                out.print("<tr style='text-align: center'><td class'columna' id='res2'>Resultado 2: </td></tr>");
+                out.print("<br>");
                 out.print("</body>");
                 out.print("</html>");
                 out.close();
--- target/Manejo_Headers-1.0/recursos/calculos.js
+++ target/Manejo_Headers-1.0/recursos/calculos.js
@@ -17,6 +17,41 @@ function Rutas(forma) {
     return true;
 }
 
+function calcularGeneral()
+{
+    var a = parseInt(document.getElementById("val_1").value);
+    var b = parseInt(document.getElementById("val_2").value);
+    var c = parseInt(document.getElementById("val_3").value);
+    
+    var res1 = document.getElementById("res1");
+    var res2 = document.getElementById("res2");
+    
+    //var primerNum = 
+    var parte1 = -b;
+    var parte3 = Math.pow(b,2);
+    var parte4 = 4*a*c;
+    var parteExtra = (parte3-parte4);
+    var parte2 = Math.sqrt(parteExtra);
+    var parte5 = 2*a;
+    
+    var solucion1 = (parte1+parte2)/(parte5);
+    var solucion2 = (parte1-parte2)/(parte5); 
+    
+    /*alert("Primera: " + parte1);
+    alert("Segunda: " + parte2);
+    alert("Extra: " + parteExtra);
+    alert("Tercera: " + parte3);
+    alert("Cuarta: " + parte4);
+    alert("Quinta: " + parte5);*/
+    res1.value=solucion1.toString();
+    res2.value=solucion2.toString();
+    
+    alert("Primera solución: " + parseFloat(solucion1));
+    alert("Segunda solución: " + parseFloat(solucion2));
+    
+}
+
+/*
 function Formula(general) {
     var num1 = general.val_1;
     var num2 = general.val_2;
@@ -49,3 +84,4 @@ function Formula(general) {
     alert("Formulario valido, enviando a servidor");
     return true;
 }
+*/
